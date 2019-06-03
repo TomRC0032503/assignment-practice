69cf3f047598267ce41f272cf8efa2e0413dedf8
RC0032503 <RC0032503@CRO-24-07>
2019-06-03 15:43:02 +0100

    added triangle

--- assignment practice/Form1.cs
+++ assignment practice/Form1.cs
@@ -16,5 +16,48 @@ namespace assignment_practice
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // calculate the area of a rectangle
+            try
+            {
+
+
+
+                double width, height, area;
+                width = Convert.ToDouble(textBox2.Text);
+                height = Convert.ToDouble(textBox1.Text);
+                area = width * height;
+                label1.Text = "area = " + area;
+            }
+            catch
+            {
+                MessageBox.Show("Please Type in 2 numbers");
+
+            }
+        }
+
+        private void tabPage3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // calculate the area of a triangle
+            try
+            {
+                double Base, height, area;
+                Base = Convert.ToDouble(textBox3.Text);
+                height = Convert.ToDouble(textBox4.Text);
+                area = Base * height / 2;
+                label6.Text = "area= " + area;
+
+
+           }
+            catch
+            { 
+              
     }
 }
